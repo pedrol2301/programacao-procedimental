a870129faac853c1950c4a2b50000c9b8c4ffe95
pedrol2301 <ppriqq@gmail.com>
2022-05-25 17:21:18 -0300

    Atualizacao

--- lista2/main.c
+++ lista2/main.c
@@ -228,6 +228,136 @@ int main()
     indicar se o estudante foi aprovado ou reprovado. A nota para aprovação deve ser igual ou
     superior a 60 pontos.
 
+
+    float a,b,c,media;
+
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+
+    c = c* 2;
+
+    media = (a + b + c)/4;
+
+    if (media >= 60){
+       printf("Aprovado\n");
+    }else{
+        printf("Reprovado\n");
+    }
+    */
+    /*
+    14. A nota final de um estudante é calculada a partir de três notas atribuídas entre o intervalo
+    de 0 até 10, respectivamente, a um trabalho de laboratório, a uma avaliação semestral e a um
+    exame final. A média das três notas mencionadas anteriormente obedece aos pesos: Trabalho
+    de Laboratório: 2; Avaliação Semestral: 3; Exame Final: 5. De acordo com o resultado, mostre na
+    tela se o aluno está reprovado (média entre 0 e 2,9), de recuperação (entre 3 e 4,9) ou se foi
+    aprovado. Faça todas as verificações necessárias.
+
+
+    float a=0,b=0,c=0,media=0;
+
+    scanf("%f",&a);
+    while(a<0 || a>10){
+        printf("Nota invalida\n");
+        scanf("%f",&a);
+    }
+    scanf("%f",&b);
+    while(b<0 || b>10){
+        printf("Nota invalida\n");
+        scanf("%f",&b);
+    }
+    scanf("%f",&c);
+    while(c<0 || c>10){
+        printf("Nota invalida\n");
+        scanf("%f",&c);
+    }
+
+    media = (a+(b*2)+(c*3))/10;
+
+     if (media == 0 && media <= 2.9){
+        printf("%.2f\n",media);
+       printf("Reprovado\n");
+    }else if(media > 3 && media <=4.9){
+        printf("%.2f\n",media);
+        printf("Recuperacao\n");
+    }else{
+        printf("%.2f\n",media);
+        printf("Aprovado\n");
+    }
+    */
+
+    /*
+    15. Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da
+    semana correspondente a este número. Isto é, domingo se 1, segunda-feira se 2, e assim por
+    diante.
+
+
+    int a;
+    scanf("%d",&a);
+    while(a<=0 || a>7){
+        printf("Dia invalido\n");
+        scanf("%d",&a);
+    }
+
+    switch(a){
+        case 1:printf("Domingo\n"); break;
+
+        case 2:printf("Segunda-Feira\n");break;
+
+        case 3:printf("Terca-Feira\n");break;
+
+        case 4:printf("Quarta-Feira\n");break;
+
+        case 5:printf("Quinta-Feira\n");break;
+
+        case 6:printf("Sexta-Feira\n");break;
+
+        case 7:printf("Sabado\n");break;
+
+        default:printf("Numero invalido\n");break;
+    }
+
+    */
+
+    /*
+        16. Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e imprima o mês
+        correspondente a este número. Isto é, janeiro se 1, fevereiro se 2, e assim por diante.
+
+
+    int a;
+    scanf("%d",&a);
+    while(a<=0 || a>12){
+        printf("Dia invalido\n");
+        scanf("%d",&a);
+    }
+
+    switch(a){
+        case 1:printf("Janeiro\n"); break;
+
+        case 2:printf("Fevereiro\n");break;
+
+        case 3:printf("Marco\n");break;
+
+        case 4:printf("Abril\n");break;
+
+        case 5:printf("Maio\n");break;
+
+        case 6:printf("Junho\n");break;
+
+        case 7:printf("Julho\n");break;
+
+        case 8:printf("Agosto\n");break;
+
+        case 9:printf("Setembro\n");break;
+
+        case 10:printf("Outubro\n");break;
+
+        case 11:printf("Novembro\n");break;
+
+        case 12:printf("Dezembro\n");break;
+
+        default:printf("Numero invalido\n");break;
+    }
     */
    system("pause");
    return 0;
