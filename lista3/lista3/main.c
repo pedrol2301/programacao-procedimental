e266aff51be79c292441cb5503f53bb196a80abb
pedrol2301 <ppriqq@gmail.com>
2022-06-01 08:33:40 -0300

    lista 3

---
+++ lista3/lista3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    1. Faça um programa que determine o mostre os cinco primeiros múltiplos de 3, considerando
+    números maiores que 0.
+    */
+
+
+    system("pause");
+    return 0;
+}
